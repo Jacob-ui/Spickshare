86c9c809aa32ea684f3c25adbda644ecad271541
CooperWoolley <s_woolley23@stud.hwr-berlin.de>
2025-06-10 16:46:19 +0200

    deleted 2 useless functions from py

--- myproject/app.py
+++ myproject/app.py
@@ -19,53 +19,6 @@ app.teardown_appcontext(db.close_db_con)
 def index():
     return redirect(url_for('login'))
 
-
-@app.route('/lists/')
-def lists():
-    db_con = db.get_db_con()
-    lists_temp = db_con.execute('SELECT * FROM list ORDER BY name').fetchall()
-    lists = []
-
-    for list_temp in lists_temp:
-        list_dict = dict(list_temp)
-        sql = (
-            'SELECT COUNT(complete) = SUM(complete) AS complete FROM todo '
-            f'JOIN todo_list ON list_id={list_dict["id"]} AND todo_id=todo.id;'
-        )
-        complete = db_con.execute(sql).fetchone()['complete']
-        list_dict['complete'] = complete
-        lists.append(list_dict)
-
-    if request.args.get('json') is not None:
-        return jsonify(lists)
-    else:
-        return render_template('lists.html', lists=lists)
-
-
-
-@app.route('/lists/<int:id>')
-def list(id):
-    db_con = db.get_db_con()
-    list_name = db_con.execute('SELECT name FROM list WHERE id=?', (id,)).fetchone()
-
-    if list_name is None:
-        return "List not found", 404
-
-    todos = db_con.execute(
-        'SELECT id, complete, description FROM todo '
-        'JOIN todo_list ON todo_id=todo.id AND list_id=? '
-        'ORDER BY id;', (id,)
-    ).fetchall()
-
-    list_data = {
-        'name': list_name['name'],
-        'todos': [dict(todo) for todo in todos]
-    }
-
-    if request.args.get('json') is not None:
-        return jsonify(list_data)
-    else:
-        return render_template('list.html', list=list_data)
     
 @app.route('/login/')
 def login():
