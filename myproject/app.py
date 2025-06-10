b2ad429a2de04fd4fd8d9f983354a992236f416c
CooperWoolley <s_woolley23@stud.hwr-berlin.de>
2025-06-10 18:29:08 +0200

    added Register.html and its funciton in app.py including error messages, doesnt work with database yet though

--- myproject/app.py
+++ myproject/app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, redirect, url_for, render_template, request, jsonify
+from flask import Flask, redirect, url_for, render_template, request, jsonify, flash
+from flask_login import login_user, login_required, logout_user, current_user
 import db  # dein db.py Modul mit DB-Funktionen
 
 app = Flask(__name__)
@@ -17,12 +18,29 @@ app.teardown_appcontext(db.close_db_con)
 
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    return render_template('index.html')
 
     
-@app.route('/login/')
+@app.route('/login/', methods = ['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template('login.html')
+
+@app.route('/register/', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if password1 != password2:
+            flash ('Passwords do not match', category='error')
+        else:
+            flash ('Registration successful!', category='success')
+            # add user to users database
+    return render_template('register.html')
 
 
 @app.route('/insert/sample')
