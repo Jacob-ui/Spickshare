09b334e2c154942ee77ae459557668b5ddb40c4b
Jacob-ui <62934892+Jacob-ui@users.noreply.github.com>
2025-06-11 13:10:11 +0200

    Voting Buttons funktionsfähig

--- myproject/app.py
+++ myproject/app.py
@@ -34,9 +34,26 @@ def register():
 
 # Liste für Sheets (Mit Sheets sind die Einträge für die CheatSheets gemeint)
 eintraege = [
-    {'titel': 'Mathe Zusammenfassung', 'beschreibung': 'Inhalte für die Klausur', 'prof': 'Müller', 'score': '6'},
-    {'titel': 'Full-Stack Web Development', 'beschreibung': 'Wie erstelle ich eine Web-App', 'prof': 'Dr. Eck, Alexander', 'score': '10'}
+    {'id': '0', 'titel': 'Mathe Zusammenfassung', 'beschreibung': 'Inhalte für die Klausur', 'prof': 'Müller', 'score': '6'},
+    {'id': '1', 'titel': 'Full-Stack Web Development', 'beschreibung': 'Wie erstelle ich eine Web-App', 'prof': 'Dr. Eck, Alexander', 'score': '10'}
 ]
+
+# Voting +/-
+@app.route("/vote", methods=["POST"])
+def vote():
+    eintrag_id = request.form.get('id') # fragt id vom sheet an
+    vote_input = request.form.get('Voteinput') # liest aus index den +/- Button aus
+    for eintrag in eintraege:
+        if eintrag['id'] == eintrag_id:
+            score = int(eintrag['score'])
+            if vote_input == '+':
+                score +=1
+            elif vote_input == '-':
+                score -=1
+            eintrag['score'] = str(score) # speichert score
+            break
+    return render_template('index.html', eintraege=eintraege)
+
 
 
 # DB
